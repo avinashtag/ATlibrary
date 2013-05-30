4aaac3f56ee3355797b32c2cfb8ce4282c2f9abe
Avinash Git <avi.tag@gmail.com>
2013-05-30 14:48:33 +0530

    Initial Commit

---
+++ ATlibrary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ATlibrary
+//
+//  Created by Vertax on 30/05/13.
+//  Copyright (c) 2013 Avinash Tag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
